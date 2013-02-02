63a443375f44441c7ec05601ee6f59a81830a7a6
yogione <smotheram@yahoo.com>
2013-02-02 13:09:53 -0500

    sat 109

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jboard::Application.config.secret_token = 'a4b75ef3b422f807bf98d26814cfbf521c61d7aebc3dbe720adfe975e7303ab7b8375eeb5ea9b41ecc0f0b34b6bd7db0b3d900ca5b3117c244f843320c655fe3'
