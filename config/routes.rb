d8bbf065437ec12d241143c96d1702670c24fcb6
yogione <smotheram@yahoo.com>
2013-02-02 20:30:33 -0500

    sat 830

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Jboard::Application.routes.draw do
+  get "home/index"
+
   resources :bids
 
   resources :posts
@@ -54,7 +56,8 @@ Jboard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'posts#index'
+#   root :to => 'posts#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
